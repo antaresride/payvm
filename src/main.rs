3365395665679843181d9e390cc4bc3060350b79
antaresride <gledsoul@gmail.com>
2026-03-25 10:02:05 -0700

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let program = vec![
+        Instruction::Load(0, 10),
+        Instruction::Load(1, 20),
+        Instruction::Add(0, 1),
+        Instruction::Print(0),
+        Instruction::Halt,
+    ];
+    let mut vm_pay = VM::new(program);
+    vm_pay.run();
+}
